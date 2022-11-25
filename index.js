830ca41b797f6c40d0884ac66b744e215fe11fc7
Abhi Raj <xl4bhi@gmail.com>
2022-11-26 03:18:44 +0530

    making a fresh start

---
+++ index.js
@@ -0,0 +1,8 @@
+// create a basic server
+
+var http = require("http");
+var server = http.createServer(function (req, res) {
+  res.writeHead(200);
+  res.end("Hello Http");
+});
+server.listen(8080);
